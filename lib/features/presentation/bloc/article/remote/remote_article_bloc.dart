18d47426e426d12a4522acb07edbf69419f053c6
RamiroCyber <ramiro.dr@gmail.com>
2023-07-30 15:11:10 -0300

    criando bloc de artigos com inversao de dependencia e states de loading success e error

---
+++ lib/features/presentation/bloc/article/remote/remote_article_bloc.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_solid/core/resources/data_state.dart';
+import 'package:flutter_solid/features/domain/usecases/get_article.dart';
+import 'package:flutter_solid/features/presentation/bloc/article/remote/remote_article_event.dart';
+import 'package:flutter_solid/features/presentation/bloc/article/remote/remote_article_state.dart';
+
+class RemoteArticlesBloc extends Bloc<RemoteArticlesEvent, RemoteArticleState> {
+  final GetArticleUseCase _getArticleUseCase;
+
+  RemoteArticlesBloc(this._getArticleUseCase)
+      : super(const RemoteArticlesLoading()) {
+    on<GetArticles>(onGetArticles);
+  }
+
+  void onGetArticles(
+      GetArticles event, Emitter<RemoteArticleState> emit) async {
+    final dataState = await _getArticleUseCase.call();
+
+    if (dataState is DataSuccess && dataState.data!.isNotEmpty) {
+      emit(RemoteArticlesLoaded(dataState.data!));
+    }
+    if (dataState is DataFailed && dataState.data!.isEmpty) {
+      emit(RemoteArticlesError(dataState.error!));
+    }
+  }
+}
